2cec4fad992d4e31c1b3e231a056774ff7c86f4f
zoltank27 <kulcsarz.nme@gmail.com>
2018-02-06 01:12:40 +0100

    AVR program changed

--- LEDCube/LEDCube/Debug/main.d
+++ LEDCube/LEDCube/Debug/main.d
@@ -13,7 +13,12 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -44,3 +49,13 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h:
--- LEDCube/LEDCube/main.c
+++ LEDCube/LEDCube/main.c
@@ -5,46 +5,102 @@
  * Author : zoltan
  */ 
 
+#define F_CPU 1000000UL
 #define DELAY 200
+
+#define LED_LOW_DDR DDRA
+#define LED_HIGH_DDR DDRC
+#define LED_LOW PORTA
+#define LED_HIGH PORTC
+#define LAYERS PORTD
+#define LAYERS_DDR DDRD
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void showFrame(uint16_t l[]) {
+static uint16_t cube[4] = {0x0001, 0x0001, 0x0001, 0x0001};
+static uint8_t matrix[4][4][4];
+
+//Beírja a LED-et a cube tömbbe, de nem kapcsolja be
+void addLED(uint8_t n) {
+	if ( (n % 16) > 7 )
+		cube[n/16] |= ( ((n%16)-8) << 2 );
+	else
+		cube[n/16] |= (n%16);
+}
+
+//Törli a LED-et a cube tömbb?l, de nem kapcsolja ki
+void removeLED(uint8_t n) {
+	if ( (n % 16) > 7 )
+		cube[n/16] &= ~( ((n%16)-8) << 2 );
+	else
+		cube[n/16] &= ~(n%16);
+}
+
+//N. LED bekapcsolása
+void setLED(uint8_t n) {
+	LAYERS = (1 << (n / 16));
+	if ( (n % 16) > 7 ) {
+		LED_HIGH |=  (1 << ( (n%16) - 8) );
+	} else {
+		LED_LOW |= (1 << (n%16));
+	}
+}
+//N. LED kikapcsolása
+void resetLED(uint8_t n) {
+	LAYERS = (1 << (n / 16));
+	if ( (n % 16) > 7 ) {
+		LED_HIGH &= ~(1 << ( (n%16) - 8) );
+	} else {
+		LED_LOW &= ~(1 << (n%16));
+	}
+}
+//LED-ek beállítása cube tömb alapján
+void showPattern() {
 	uint8_t i;
 	for (i = 0; i < 4; i++) {
-		PORTD = (1 << i);
-		PORTA = l[i] & 0x00FF;
-		PORTC = (l[i] & 0xFF00) >> 8;
-		_delay_ms(4);
+		LAYERS = (1 << i);
+		LED_LOW = cube[i] & 0x00FF;
+		LED_HIGH = (cube[i] & 0xFF00) >> 8;
+		//_delay_ms(4);
 	}
 }
 
+//
+
 int main(void)
 {
-    /* Replace with your application code */
-	//A, C portok --> kimenet
-	DDRA = 0xFF;
-	DDRC = 0xFF;
-	DDRD = 0xFF;
+	//A, C, D portok --> kimenet
+	LED_LOW_DDR = 0xFF;	//LED-ek 0..7
+	LED_HIGH_DDR = 0xFF;	//LED-ek 8..15
+	LAYERS_DDR = 0x0F;	//Rétegek 0..4
 	
-	MCUCSR |=(1<<JTD);
-	MCUCSR |=(1<<JTD);
-
-	uint16_t layer[4] = {0x0001, 0x0001, 0x0001, 0x0001};
-	cli();
+	//JTAG kikapcsolva
+	MCUCSR |= (1<<JTD);
 	
-	uint8_t t = 0;
+	uint8_t r1, r2;
+	time_t t;
+	srand((unsigned) time(&t));
     while (1)
     {
-		for (t = 0; t < 63; t++) {		//63*~16 = 1000 msec
-			showFrame(layer);
-		}
-		layer[0] = layer[0] << 1;
-		layer[1] = layer[1] << 1;
-		layer[2] = layer[2] << 1;
-		layer[3] = layer[3] << 1;
+		r1 = rand()%63;
+		setLED(r1);
+		_delay_ms(300);
+		
+		r2 = rand()%63;
+		setLED(r2);
+		_delay_ms(300);
+		
+		resetLED(r1);
+		_delay_ms(300);
+		
+		resetLED(r2);
+		_delay_ms(300);
+		
 	}
 }
 
